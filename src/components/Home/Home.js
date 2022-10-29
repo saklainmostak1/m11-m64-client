9131f3ae31516a46250b0e3dbbd9c8b71a978631
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-30 05:44:25 +0600

    push

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData()
+    const [displayUsers, setDisplayUsers] = useState(users)
 
     const handleDelete  = user =>{
-        const agree = window.confirm(`Are You Sure ${user.name}`)
+        const agree = window.confirm(`Are You Sure delete ${user.name}`)
         if(agree){
-            
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE',
+            })
+            .then(Response => Response.json())
+            .then(data => {
+                // console.log(data)
+                if(data.deletedCount > 0){
+                    alert('user delete')
+                    const remainingUsers = displayUsers.filter(usr => usr._id !== user._id)
+                    setDisplayUsers(remainingUsers)
+                }
+            })
         }
 
     }
     return (
         <div>
-            <h2>Users: {users.length} </h2>
+            <h2>Users: {displayUsers.length} </h2>
             <div>
                 {
-                    users.map(user => <p
+                    displayUsers.map(user => <p
                     key={user._id}
                     >
                         {user.name}
-                        {user.adress?.adress}
                         {user.email} <button
                          onClick={() => handleDelete(user)}>X</button>
                     </p>)
